71ca390a5b31c9f0a4026d075187366b0ef2f87f
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-03-29 15:39:12 -0300

    Demuestra que heredó todo lo definido por el padre

---
+++ src/calificaciones/PruebaNotaDeTP.java
@@ -0,0 +1,27 @@
+package calificaciones;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PruebaNotaDeTP {
+
+	@Test
+	public void creaLaNotaDeTPConValorCero() {
+		
+		NotaDeTP notaDeTP = new NotaDeTP();
+		
+		int valor = notaDeTP.obtenerValor();
+		
+		Assert.assertEquals(0, valor);
+	}
+	
+	@Test
+	public void asignarValor8() {
+		
+		NotaDeTP notaDeTP = new NotaDeTP();
+		
+		notaDeTP.asignar(8);
+		
+		Assert.assertEquals(8, notaDeTP.obtenerValor());
+	}
+}
